4cf428aeff4819911bb6309a27b4d51c01b62249
Alfredo Di Napoli <alfredo.dinapoli@iohk.io>
2018-02-05 18:12:16 +0100

    Install stack if not there

--- DnsCheck.hs
+++ DnsCheck.hs
@@ -3,6 +3,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Shelly
+import Data.Maybe
+import Data.Monoid
 import System.Environment (getArgs)
 import qualified Data.Text as T
 
@@ -11,17 +13,36 @@ type GitHash = T.Text
 data DnsLibVersion = VR_3_0_1
                    | VR_git !GitHash
 
+renderLibVersion :: DnsLibVersion -> T.Text
+renderLibVersion VR_3_0_1 = "3.0.1"
+renderLibVersion (VR_git hash) = hash
+
 cardanoCurrentRevision :: GitHash
 cardanoCurrentRevision = "b106470f0a93672af22cbc7ed6564b53c0f249ed"
 
 main :: IO ()
 main = do
   args <- getArgs
-  shelly $ silently $ do
+  shelly $ verbosely $ do
     case args of
       ["3.0.1"] -> dnsCheck VR_3_0_1
       _ -> dnsCheck (VR_git cardanoCurrentRevision)
 
+isStackInstalled :: IO Bool
+isStackInstalled = shelly $ silently $ errExit False $ do
+  p <- fromMaybe mempty <$> which "stack"
+  ec <- lastExitCode
+  pure (ec == 0 && (not $ T.null (toTextIgnore p)))
 
 dnsCheck :: DnsLibVersion -> Sh ()
-dnsCheck _ = echo "Hello World!"
+dnsCheck vr = do
+  let libVersion = renderLibVersion vr
+  echo $ ">> Testing DNS lookups against dns library: " <> libVersion
+  stackInstalled <- liftIO isStackInstalled
+  -- Install stack if not there.
+  unless stackInstalled $ escaping False $
+    run_ "curl" ["-sSL", "https://get.haskellstack.org/", "|", "sh"]
+
+  -- Fetch the right project, build it and run the check.
+  chdir (fromText $ "dns-check-" <> libVersion) $ do
+    run_ "stack" ["build"]
